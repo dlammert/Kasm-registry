a7661a696f2abce8d96a1333e2ec4ba615ff09f1
Boomsalot <46827210+dlammert@users.noreply.github.com>
2025-02-04 15:08:56 -0600

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Airgap Kasm Technologies',
+    description: 'The unofficial store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: ' https://dlammert.github.io/kasm-registry/',
+    contactUrl: 'https://none.com',
   },
   reactStrictMode: true,
   swcMinify: true,
